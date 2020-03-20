36d937a9c257e57e9ee98b5e0d3441758dfd5de9
lizq <362608691@qq.com>
2020-03-20 14:04:23 +0800

    --A 这是我的第一次提交的代码

---
+++ src/main/java/com/lening/test.java
@@ -0,0 +1,11 @@
+package com.lening;
+
+/**
+ * 创作时间：2020/3/20 13:59
+ * 作者：李增强
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("111111");
+    }
+}
